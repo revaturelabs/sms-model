bb0f7e747f3bfb9d7bad059ec60110ad5eb19ecf
rorr <rlayneorr@aim.com>
2018-04-11 14:32:26 -0400

    adding some public members so that I can test nexus repo

--- src/main/java/com/revature/sms/beans/Trainee.java
+++ src/main/java/com/revature/sms/beans/Trainee.java
@@ -3,6 +3,77 @@ package com.revature.sms.beans;
 import org.springframework.hateoas.ResourceSupport;
 
 public class Trainee extends ResourceSupport {
-	private Long id;
+	private Long traineeId;
 	private String email;
+	private Long batch;
+	
+	public Trainee() {
+		super();
+	}
+
+	public Long getTraineeId() {
+		return traineeId;
+	}
+
+	public void setTraineeId(Long traineeId) {
+		this.traineeId = traineeId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getBatch() {
+		return batch;
+	}
+
+	public void setBatch(Long batch) {
+		this.batch = batch;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((batch == null) ? 0 : batch.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((traineeId == null) ? 0 : traineeId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trainee other = (Trainee) obj;
+		if (batch == null) {
+			if (other.batch != null)
+				return false;
+		} else if (!batch.equals(other.batch))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (traineeId == null) {
+			if (other.traineeId != null)
+				return false;
+		} else if (!traineeId.equals(other.traineeId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Trainee [traineeId=" + traineeId + ", email=" + email + ", batch=" + batch + "]";
+	}
 }
