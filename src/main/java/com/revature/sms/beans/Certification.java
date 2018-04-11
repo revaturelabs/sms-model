3befd049ae223d981c98e23d78e2de20da3f5f6a
rorr <rlayneorr@aim.com>
2018-04-11 14:01:41 -0400

    minor change to trigger build

--- src/main/java/com/revature/sms/beans/Certification.java
+++ src/main/java/com/revature/sms/beans/Certification.java
@@ -1,7 +1,9 @@
 package com.revature.sms.beans;
 
+import java.sql.Date;
+
 import org.springframework.hateoas.ResourceSupport;
 
 public class Certification extends ResourceSupport{
-
+	private Date deadline;
 }
